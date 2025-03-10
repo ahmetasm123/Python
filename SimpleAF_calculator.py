7c6009a945720d574b39e9f8b4fba7ff1563c1a8
ahmetasm123 <125503966+ahmetasm123@users.noreply.github.com>
2025-03-10 18:02:23 +0300

    Update SimpleAF_calculator.py

--- SimpleAF_calculator.py
+++ SimpleAF_calculator.py
@@ -12,21 +12,21 @@ def div(st,nd):
     res = st / nd
 
 while True:
- calc= str(input("whats the function= "))
- st= int(input("your first number= "))
- nd= int(input("your second number= "))
- eq="="
- match calc:
-  case "+":
-      add(st,nd)
-      clc = "+"
-  case "-":
-      sub(st,nd)
-      clc = "-"
-  case "/":
-      div(st,nd)
-      clc = "/"
-  case "*":
-      mul(st,nd)
-      clc = "*"
- print(st, clc, nd, eq, res)
+    calc= str(input("whats the function= "))
+    st= int(input("your first number= "))
+    nd= int(input("your second number= "))
+    eq="="
+    match calc:
+        case "+":
+          add(st,nd)
+          clc = "+"
+        case "-":
+          sub(st,nd)
+          clc = "-"
+        case "/":
+          div(st,nd)
+          clc = "/"
+        case "*":
+          mul(st,nd)
+          clc = "*"
+    print(st, clc, nd, eq, res)
